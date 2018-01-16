3b609f0ad2f2ff9d253ceec058bb982b5812b93c
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-01-16 14:28:58 -0700

    Enable OpenMP for ATDM GCC 7.2.0 build (#2028)
    Word from Matt B. is that OpenMP should be enabled for this build.

--- cmake/std/sems/atdm/SEMSATDMSettings.cmake
+++ cmake/std/sems/atdm/SEMSATDMSettings.cmake
@@ -2,6 +2,8 @@
 # These are special setting for the ATDM configuration of Trilinos using the SEMS 
 #
 
+SET(${PROJECT_NAME}_ENABLE_OpenMP ON CACHE BOOL "Set in SEMSATDMSettings.cmake")
+
 # ATDM builds of Trilinos don't need HDF5 support in EpetraExt and this avoids
 # a build error with GCC 7.2.0 (see #2080)
 SET(EpetraExt_ENABLE_HDF5 OFF CACHE BOOL "Set in SEMSATDMSettings.cmake")
