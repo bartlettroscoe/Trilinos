7374a63f2501fbf92410423c10571a8d5d11617a
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-12-15 07:35:38 -0700

    WIP: Swtich gnu-4.9.3 build to not use sparc modules (TRIL-212)
    In order for others to use the SPARC-installed TPLs, it was requested
    not to allow non-SPARC developers to use SPARC modules.
    
    This just changes over the gnu-4.9.3 compiler and TPLs.  Hopefully
    other compilers will have a lot of commonality and therefore allow
    for a lot of reuse.

--- cmake/std/atdm/cee-rhel6/environment.sh
+++ cmake/std/atdm/cee-rhel6/environment.sh
@@ -10,11 +10,12 @@
 # used so no need for dealing with different varients of compilers here.
 
 if [[ "$ATDM_CONFIG_COMPILER" == "DEFAULT" ]] ; then
-  # Abort, no compiler was selected!
+  # No supported compiler was selected in custom_bulids.sh so abort!
   return
 fi
 
 if [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "DEFAULT" ]] ; then
+  # Use no KOKKOS_ARCH so it will not appear in the tweaks file
   unset ATDM_CONFIG_KOKKOS_ARCH
 else
   echo
@@ -45,7 +46,40 @@ fi
 export ATDM_CONFIG_BUILD_COUNT=$ATDM_CONFIG_MAX_NUM_CORES_TO_USE
 # NOTE: Use as many build processes and there are cores by default.
 
+#
+# Save the original PATH and LD_LIBRARY_PATH if not already saved
+#
+
+if [[ "${ATDM_PATH_ORIG}" == "" ]] ; then
+  export ATDM_PATH_ORIG=$PATH
+  export ATDM_LD_LIBRARY_PATH_ORIG=${LD_LIBRARY_PATH}
+fi
+
+#
+# Purge as much as you can
+#
+
 module purge
+
+unset BOOST_ROOT
+unset HDF5_ROOT
+unset CGNS_ROOT
+unset PNETCDF_ROOT
+unset NETCDF_ROOT
+unset LIBHIO_ROOT
+unset METIS_ROOT
+unset PARMETIS_ROOT
+unset SUPERLUDIST_ROOT
+unset SGM_ROOT
+unset EUCLID_ROOT
+unset SUPERLUDIST_ROOT
+
+export PATH=$ATDM_PATH_ORIG
+export LD_LIBRARY_PATH=$ATDM_LD_LIBRARY_PATH_ORIG
+
+#
+# Set up basic number of processes to use for building and running tests
+#
 
 if [[ "$ATDM_CONFIG_NODE_TYPE" == "OPENMP" ]] ; then
   export ATDM_CONFIG_CTEST_PARALLEL_LEVEL=$(($ATDM_CONFIG_MAX_NUM_CORES_TO_USE/2))
@@ -55,7 +89,13 @@ else
 fi
 # NOTE: Above, we use 1/2 as many executors as
 
+
+#
+# Set up the env based on the chosen compiler
+#
+
 if [ "$ATDM_CONFIG_COMPILER" == "CLANG-5.0.1-OPENMPI-1.10.2" ]; then
+
   module load sparc-dev/clang-5.0.1_openmpi-1.10.2
   export OMPI_CXX=`which clang++`
   export OMPI_CC=`which clang`
@@ -63,7 +103,9 @@ if [ "$ATDM_CONFIG_COMPILER" == "CLANG-5.0.1-OPENMPI-1.10.2" ]; then
   export MPICC=`which mpicc`
   export MPICXX=`which mpicxx`
   export MPIF90=`which mpif90`
+
 elif [[ "$ATDM_CONFIG_COMPILER" == "GNU-7.2.0-OPENMPI-1.10.2" ]] ; then
+
   module load sparc-dev/gcc-7.2.0_openmpi-1.10.2
   export OMPI_CXX=`which g++`
   export OMPI_CC=`which gcc`
@@ -75,19 +117,38 @@ elif [[ "$ATDM_CONFIG_COMPILER" == "GNU-7.2.0-OPENMPI-1.10.2" ]] ; then
   export ATDM_CONFIG_MPI_EXEC_NUMPROCS_FLAG=-np
   export ATDM_CONFIG_MPI_POST_FLAGS="-bind-to;core"
   # NOTE: Above is waht What SPARC uses?
+
 elif [[ "$ATDM_CONFIG_COMPILER" == "GNU-4.9.3-OPENMPI-1.10.2" ]] ; then
-  module load sparc-dev/gcc-4.9.3_openmpi-1.10.2
-  export OMPI_CXX=`which g++`
+
+  module load tools/intel-advixe-2016.1
+  module load tools/intel-amplxe-2016.1
+  module load tools/intel-inspxe-2016.1
+  SPARC_TPL_ROOT=/projects/sparc/tpls/cee-rhel6
+  SPARC_TPL_BUILD_NAME=cee-cpu_gcc-4.9.3
+  SPARC_MPI_TPL_BUILD_NAME=cee-cpu_gcc-4.9.3_openmpi-1.10.2
+  export CBLAS_ROOT=/sierra/sntools/SDK/compilers/intel/composer_xe_2017.1.132/compilers_and_libraries/linux
+  export GNU_ROOT=/sierra/sntools/SDK/compilers/gcc/4.9.3-RHEL6
+  export MPI_ROOT=/sierra/sntools/SDK/mpi/openmpi/1.10.2-gcc-4.9.3-RHEL6
+  export PATH=${MPI_ROOT}/bin:${GNU_ROOT}/bin:$PATH
+  export LD_LIBRARY_PATH=${GNU_ROOT}/lib64:${GNU_ROOT}/lib:${LD_LIBRARY_PATH}
+  export LD_LIBRARY_PATH=$MPI_ROOT}/lib:/sierra/sntools/SDK/hwloc/lib:${LD_LIBRARY_PATH}
+  export CC=`which gcc`
+  export CXX=`which g++`
+  export FC=`which gfortran`
   export OMPI_CC=`which gcc`
+  export OMPI_CXX=`which g++`
   export OMPI_FC=`which gfortran`
   export MPICC=`which mpicc`
   export MPICXX=`which mpicxx`
   export MPIF90=`which mpif90`
   export ATDM_CONFIG_MPI_PRE_FLAGS="--bind-to;none"
-  # Still uses old 
+  # Still uses old TPL for SuperLUDist
+  export SUPERLUDIST_ROOT=${SPARC_TPL_ROOT}/superlu_dist-4.2/${SPARC_MPI_TPL_BUILD_NAME}
   export ATDM_CONFIG_SUPERLUDIST_INCLUDE_DIRS=${SUPERLUDIST_ROOT}/SRC
   export ATDM_CONFIG_SUPERLUDIST_LIBS=${SUPERLUDIST_ROOT}/lib/libsuperlu_dist_4.2.a
+
 elif [ "$ATDM_CONFIG_COMPILER" == "INTEL-18.0.2-MPICH2-3.2" ]; then
+
   module load sparc-dev/intel-18.0.2_mpich2-3.2
   export OMPI_CXX=`which icpc`
   export OMPI_CC=`which icc`
@@ -101,7 +162,9 @@ elif [ "$ATDM_CONFIG_COMPILER" == "INTEL-18.0.2-MPICH2-3.2" ]; then
   export ATDM_CONFIG_OPENMP_FORTRAN_FLAGS=-fopenmp
   export ATDM_CONFIG_OPENMP_FORTRAN_LIB_NAMES=gomp
   export ATDM_CONFIG_OPENMP_GOMP_LIBRARY=-lgomp
+
 elif [ "$ATDM_CONFIG_COMPILER" == "INTEL-17.0.1-INTELMPI-5.1.2" ]; then
+
   module load sparc-dev/intel-17.0.1_intelmpi-5.1.2
   export OMPI_CXX=`which icpc`
   export OMPI_CC=`which icc`
@@ -114,12 +177,15 @@ elif [ "$ATDM_CONFIG_COMPILER" == "INTEL-17.0.1-INTELMPI-5.1.2" ]; then
   export ATDM_CONFIG_OPENMP_FORTRAN_FLAGS=-fopenmp
   export ATDM_CONFIG_OPENMP_FORTRAN_LIB_NAMES=gomp
   export ATDM_CONFIG_OPENMP_GOMP_LIBRARY=-lgomp
+
 else
+
   echo
   echo "***"
   echo "*** ERROR: COMPILER=$ATDM_CONFIG_COMPILER is not supported on this system!"
   echo "***"
   return
+
 fi
 
 # ToDo: Update above to only load the compiler and MPI moudles and then
@@ -130,6 +196,44 @@ fi
 module load atdm-env
 module load atdm-cmake/3.11.1
 module load atdm-ninja_fortran/1.7.2
+
+#
+# Set up TPL ROOT env vars
+#
+
+export BOOST_ROOT=${SPARC_TPL_ROOT}/boost-1.59.0/${SPARC_TPL_BUILD_NAME}
+export HDF5_ROOT=${SPARC_TPL_ROOT}/hdf5-1.8.16/${SPARC_MPI_TPL_BUILD_NAME}
+export CGNS_ROOT=${SPARC_TPL_ROOT}/CGNS/${SPARC_MPI_TPL_BUILD_NAME}_hdf5-1.8.16
+export PNETCDF_ROOT=${SPARC_TPL_ROOT}/pnetcdf-1.6.1/${SPARC_MPI_TPL_BUILD_NAME}
+export NETCDF_ROOT=${SPARC_TPL_ROOT}/netcdf-4.4.0/${SPARC_MPI_TPL_BUILD_NAME}_hdf5-1.8.16
+export METIS_ROOT=${SPARC_TPL_ROOT}/parmetis-4.0.3/${SPARC_MPI_TPL_BUILD_NAME}
+export PARMETIS_ROOT=${SPARC_TPL_ROOT}/parmetis-4.0.3/${SPARC_MPI_TPL_BUILD_NAME}
+export SUPERLUDIST_ROOT=${SPARC_TPL_ROOT}/superlu_dist-4.2/${SPARC_MPI_TPL_BUILD_NAME}
+
+# ToDo: Can we remove these below for Trilinos?  Are these only needed for SPARC?
+export LIBHIO_ROOT=${SPARC_TPL_ROOT}/libhio-1.4.1.2/cee_gcc-4.9.3_openmpi-1.10.2
+export SGM_ROOT=${SPARC_TPL_ROOT}/sgm/${SPARC_MPI_TPL_BUILD_NAME}
+export EUCLID_ROOT=${SPARC_TPL_ROOT}/euclid/${SPARC_MPI_TPL_BUILD_NAME}
+
+#
+# Set paths to executbles and libraries
+#
+
+export LD_LIBRARY_PATH=/usr/netpub/lld/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${CBLAS_ROOT}/mkl/lib/intel64:${LD_LIBRARY_PATH}
+
+export PATH=${BOOST_ROOT}/bin:${PATH}
+export PATH=${HDF5_ROOT}/bin:${PATH}
+export PATH=${CGNS_ROOT}/bin:${PATH}
+export PATH=${PNETCDF_ROOT}/bin:${PATH}
+export PATH=${NETCDF_ROOT}/bin:${PATH}
+
+# ToDo: Can we remove this below for Trilinos?  Are these only needed by SPARC?
+export PATH=${LIBHIO_ROOT}/bin:${PATH}
+
+#
+# Set TPL include dirs and libs
+#
 
 export ATDM_CONFIG_USE_HWLOC=OFF
 
@@ -155,14 +259,10 @@ atdm_config_add_libs_to_var ATDM_CONFIG_BLAS_LIBS ${CBLAS_ROOT}/lib/intel64 .so 
 
 export ATDM_CONFIG_LAPACK_LIBS=${ATDM_CONFIG_BLAS_LIBS}
 
-# NOTE: HDF5_ROOT and NETCDF_ROOT should already be set in env from above
-# module loads!
-
-# However, set the direct libs for HDF5 and NetCDF in case we use that option
-# for building (see env var ATDM_CONFIG_USE_SPARC_TPL_FIND_SETTINGS).
-
+# HDF5
 export ATDM_CONFIG_HDF5_LIBS="-L${HDF5_ROOT}/lib;${HDF5_ROOT}/lib/libhdf5_hl.a;${HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl"
 
+# Netcdf
 export ATDM_CONFIG_NETCDF_LIBS="-L${BOOST_ROOT}/lib;-L${NETCDF_ROOT}/lib;-L${NETCDF_ROOT}/lib;-L${SEMS_PNETCDF_ROOT}/lib;-L${HDF5_ROOT}/lib;${BOOST_ROOT}/lib/libboost_program_options.a;${BOOST_ROOT}/lib/libboost_system.a;${NETCDF_ROOT}/lib/libnetcdf.a;${PNETCDF_ROOT}/lib/libpnetcdf.a;${HDF5_ROOT}/lib/libhdf5_hl.a;${HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl;-lcurl"
 
 # SuperLUDist
