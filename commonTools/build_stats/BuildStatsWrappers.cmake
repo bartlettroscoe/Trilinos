27dd80209df661abf237c512e9ab38085a6fff40
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-06-09 16:58:17 -0600

    WIP: Add install hooks for compiler wrappers and gather script (#7376)
    I also changed the logic in how the compiler wrappers are generated a little.

--- commonTools/build_stats/BuildStatsWrappers.cmake
+++ commonTools/build_stats/BuildStatsWrappers.cmake
@@ -1,4 +1,12 @@
+################################################################################
+#
+# Set up for build stats compiler wrappers and gathering up build stats
+#
+################################################################################
+
+
 set(BUILD_STATS_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}")
+
 
 # Generate the build stats compiler wrappers if asked to do so.
 #
@@ -18,39 +26,75 @@ function(generate_build_stats_wrappers)
       generate_build_stats_wrapper_for_lang(Fortran)
     endif()
 
+    set(gather_build_status "${${PROJECT_NAME}_BINARY_DIR}/gather_build_stats.sh")
     configure_file("${BUILD_STATS_SRC_DIR}/gather_build_stats.sh"
-      "${${PROJECT_NAME}_BINARY_DIR}/gather_build_stats.sh" COPYONLY)
+      "${gather_build_status}" COPYONLY)
 
   endif()
 
 endfunction()
 
 
+# Generate the build stats compiler wrapper for a single language.
+#
 function(generate_build_stats_wrapper_for_lang  lang)
 
-  message("generate_build_stats_wrapper_for_lang: ${lang}")
+  string(TOLOWER "${lang}" lang_lc)
+  set(compiler_wrapper
+    "${${PROJECT_NAME}_BINARY_DIR}/build_stat_${lang_lc}_wrapper.sh")
 
+  # Override the compiler with the wrapper but remember the original compiler
   if ("${CMAKE_${lang}_COMPILER_ORIG}" STREQUAL "")
-
-    message("-- " "Generated build stats compiler wrapper for ${lang}")
-
     set(CMAKE_${lang}_COMPILER_ORIG "${CMAKE_${lang}_COMPILER}"
       CACHE FILEPATH "Original non-wrappeed ${lang} compiler" FORCE )
-
-    set(BUILD_STAT_COMPILER_WRAPPER_INNER_COMPILER "${CMAKE_${lang}_COMPILER}")
-
-    string(TOLOWER "${lang}" lang_lc)
-    set(compiler_wrapper
-      "${${PROJECT_NAME}_BINARY_DIR}/build_stat_${lang_lc}_wrapper.sh")
-
-    configure_file("${BUILD_STATS_SRC_DIR}/build_stat_lang_wrapper.sh.in"
-      "${compiler_wrapper}" @ONLY)
-
     set(CMAKE_${lang}_COMPILER "${compiler_wrapper}"
       CACHE FILEPATH "Overwritten build stats ${lang} compiler wrapper" FORCE )
+  endif()
+
+  message("-- " "Generated build stats compiler wrapper for ${lang}")
+  set(BUILD_STAT_COMPILER_WRAPPER_INNER_COMPILER "${CMAKE_${lang}_COMPILER_ORIG}")
+  configure_file("${BUILD_STATS_SRC_DIR}/build_stat_lang_wrapper.sh.in"
+    "${compiler_wrapper}" @ONLY)
+
+endfunction()
+
+# NOTE: The above implementation will make sure the compiler wrapper will get
+# updated if the *.sh.in template file changes and just reconfiguring.
+# Actaully, you should be able to fix the wrapper and just type 'make' and it
+# should reconfigure and update automatically.
+
+
+# Set up install targets for the build stats scripts
+#
+# This can only be called after these install dirs have been set!
+#
+function(install_build_stats_scripts)
+
+  if (${PROJECT_NAME}_USE_BUILD_PERF_WRAPPERS)
+
+    set(gather_build_status "${${PROJECT_NAME}_BINARY_DIR}/gather_build_stats.sh")
+    install(PROGRAMS "${gather_build_status}" 
+      DESTINATION "${${PROJECT_NAME}_INSTALL_RUNTIME_DIR}")
+
+    install_build_stats_wrapper_for_lang(C)
+    install_build_stats_wrapper_for_lang(CXX)
+    if (${PROJECT_NAME}_ENABLE_Fortran)
+      install_build_stats_wrapper_for_lang(Fortran)
+    endif()
 
   endif()
 
+endfunction()
+
+
+# Install the build stats compiler wrapper for a single language.
+#
+function(install_build_stats_wrapper_for_lang  lang)
+  string(TOLOWER "${lang}" lang_lc)
+  set(compiler_wrapper
+    "${${PROJECT_NAME}_BINARY_DIR}/build_stat_${lang_lc}_wrapper.sh")
+  install(PROGRAMS "${compiler_wrapper}"
+    DESTINATION "${${PROJECT_NAME}_INSTALL_RUNTIME_DIR}")
 endfunction()
 
 
