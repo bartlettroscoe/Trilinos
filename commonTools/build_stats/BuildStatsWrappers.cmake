246d99cf4b7b6a5815c2e1bfed9cc58139a87353
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-06-11 17:35:39 -0600

    WIP: Add dependencies on all build targets for 'gather-build-stats' (#7376)

--- commonTools/build_stats/BuildStatsWrappers.cmake
+++ commonTools/build_stats/BuildStatsWrappers.cmake
@@ -119,9 +119,68 @@ function(add_target_gather_build_stats)
       COMMAND "${${PROJECT_NAME}_BINARY_DIR}/gather_build_stats.sh"
       WORKING_DIRECTORY "${${PROJECT_NAME}_BINARY_DIR}" )
 
-    add_custom_target(gather-build-stats
+    add_custom_target(gather-build-stats ALL
       DEPENDS "${buildStatsCsvFile}")
 
+    get_all_targets_including_in_subdirs("${${PROJECT_NAME}_SOURCE_DIR}"
+      projectTargetsList)
+
+    print_var(projectTargetsList)
+
+    add_dependencies(gather-build-stats ${projectTargetsList})
+
   endif()
+
+endfunction()
+
+
+# Get a list all of the lib and exec targets in a subdir on below.
+#
+function(get_all_targets_including_in_subdirs srcdir  targetsListVarOut)
+
+  set(targetsList "")
+
+  # Recurse into subdirectories.
+  get_property(dirs DIRECTORY ${srcdir} PROPERTY SUBDIRECTORIES)
+  foreach(d IN LISTS dirs)
+    get_all_targets_including_in_subdirs(${d} targetsSubdirList)
+    list(APPEND targetsList ${targetsSubdirList})
+  endforeach()
+
+  # Get the targets from this directory.
+  get_property(allTargetsThisDir DIRECTORY ${srcdir} PROPERTY BUILDSYSTEM_TARGETS)
+  filter_only_build_targets(allTargetsThisDir buildTargetsThisDir)
+  list(APPEND targetsList ${buildTargetsThisDir})
+
+  # Return
+  set(${targetsListVarOut} ${targetsList} PARENT_SCOPE)
+
+endfunction()
+
+
+function(filter_only_build_targets targetListInVar targetListOutVar)
+
+  #print_var(targetListInVar)
+  #print_var(${targetListInVar})
+
+  set(targetListOut "")
+
+  foreach (target IN LISTS ${targetListInVar})
+    #print_var(target)
+    get_property(targetType TARGET ${target} PROPERTY TYPE)
+    #print_var(targetType)
+    if (
+        targetType STREQUAL "STATIC_LIBRARY" OR
+        targetType STREQUAL "SHARED_LIBRARY" OR
+        targetType STREQUAL "EXECUTABLE"
+      )
+      #message("-- " "${target} is a regular build target!")
+      list(APPEND targetListOut ${target})
+    else()
+      #message("-- " "${target} is **NOT** a regular build target!")
+    endif()
+  endforeach()
+
+  set(${targetListOutVar} ${targetListOut} PARENT_SCOPE)
 
 endfunction()
