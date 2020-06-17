8ed53079be48e4b9463e37d31b7f9535a89e2936
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-06-17 11:44:17 -0600

    SQUASH AGAINST: Turn on build stats for sems-rhel6 and sems-rhel7 builds (#7376)

--- cmake/ctest/drivers/atdm/sems-rhel6/local-driver.sh
+++ cmake/ctest/drivers/atdm/sems-rhel6/local-driver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 
+set +x
+
 if [[ "${Trilinos_ENABLE_BUILD_STATS}" == "" ]] ; then
   export Trilinos_ENABLE_BUILD_STATS=ON
 fi
-
-set -x
 
 $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ctest-s-driver.sh
