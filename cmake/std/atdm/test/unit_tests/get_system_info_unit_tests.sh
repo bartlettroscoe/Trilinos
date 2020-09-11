84543869e0fb2595fba6f784f231bcaf12396800
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-09-11 17:20:15 -0600

    WIP: Add unit tests for custom systems (#8008)
    I did not add the test for a ATDM_CONFIG_REGISTER_CUSTOM_CONFIG_DIR with no
    default known system yet.  I will do that next.

--- cmake/std/atdm/test/unit_tests/get_system_info_unit_tests.sh
+++ cmake/std/atdm/test/unit_tests/get_system_info_unit_tests.sh
@@ -16,7 +16,8 @@ else
   SHUNIT2_DIR=`readlink -f ${ATDM_CONFIG_SCRIPT_DIR}/../../../commonTools/test/shunit2`
 fi
 
-ATDM_UNIT_TESTS_DIR=${ATDM_CONFIG_SCRIPT_DIR}/test/unit_tests/
+ATDM_UNIT_TESTS_DIR=${ATDM_CONFIG_SCRIPT_DIR}/test/unit_tests
+
 
 #
 # Unit test helper functions
@@ -37,6 +38,14 @@ function init_system_info_test_env() {
   ATDM_CONFIG_GET_KNOW_SYSTEM_INFO_REAL_HOSTNAME_OVERRIDE_FOR_UNIT_TESTING=${realhostname}
   ATDM_CONFIG_BUILD_NAME=default
   ATDM_CONFIG_DISABLE_WARNINGS=ON
+  unset ATDM_CONFIG_CUSTOM_CONFIG_DIR_ARG
+  unset ATDM_CONFIG_REGISTER_CUSTOM_CONFIG_DIR
+  unset ATDM_CONFIG_SYSTEM_NAME
+  unset ATDM_CONFIG_REAL_HOSTNAME
+  unset ATDM_CONFIG_CDASH_HOSTNAME
+  unset ATDM_CONFIG_SYSTEM_NAME
+  unset ATDM_CONFIG_SYSTEM_DIR
+  unset ATDM_CONFIG_CUSTOM_CONFIG_DIR
 }
 
 
@@ -46,16 +55,29 @@ function assert_system_info_output() {
   expected_cdash_hostname=$2
   expected_system_name=$3
   # Body
+  ${_ASSERT_EQUALS_} '"${realhostname}"'  '"${ATDM_CONFIG_REAL_HOSTNAME}"'
+  ${_ASSERT_EQUALS_} '"${expected_cdash_hostname}"'  '"${ATDM_CONFIG_CDASH_HOSTNAME}"'
+  ${_ASSERT_EQUALS_} '"${expected_system_name}"'  '"${ATDM_CONFIG_SYSTEM_NAME}"'
+  ${_ASSERT_EQUALS_} '"${ATDM_CONFIG_SCRIPT_DIR}/${expected_system_name}"' \
+    '"${ATDM_CONFIG_SYSTEM_DIR}"'
+  ${_ASSERT_EQUALS_} '""' '"${ATDM_CONFIG_CUSTOM_SYSTEM_DIR}"'
+}
+
+
+function assert_custom_system_info_output() {
+  # Args
+  realhostname=$1
+  expected_cdash_hostname=$2
+  expected_system_name=$3
+  expected_system_dir=$4
+  # Body
   assertEquals ${expected_system_name}  "${ATDM_CONFIG_SYSTEM_NAME}"
-  # NOTE: ABove works even if arg is empty to help show this was not set!
-  ${_ASSERT_EQUALS_} ${realhostname} "${ATDM_CONFIG_REAL_HOSTNAME}"
-  ${_ASSERT_EQUALS_} ${expected_cdash_hostname} "${ATDM_CONFIG_CDASH_HOSTNAME}"
-  ${_ASSERT_EQUALS_} ${expected_system_name}  "${ATDM_CONFIG_SYSTEM_NAME}"
-  ${_ASSERT_EQUALS_} "${ATDM_CONFIG_SCRIPT_DIR}/${expected_system_name}" \
-    "${ATDM_CONFIG_SYSTEM_DIR}"
-  assertEquals "" "${ATDM_CONFIG_CUSTOM_SYSTEM_DIR}"
-  # NOTE: Have to use assertEquals instead of macro ${_ASSERT_EQUALS_} beause
-  # latter does not work with empty values?
+  # NOTE: Above works even if arg is empty to help show this was not set!
+  ${_ASSERT_EQUALS_} '"${realhostname}"'  '"${ATDM_CONFIG_REAL_HOSTNAME}"'
+  ${_ASSERT_EQUALS_} '"${expected_cdash_hostname}"'  '"${ATDM_CONFIG_CDASH_HOSTNAME}"'
+  ${_ASSERT_EQUALS_} '"${expected_system_name}"'  '"${ATDM_CONFIG_SYSTEM_NAME}"'
+  ${_ASSERT_EQUALS_} '"${expected_system_dir}"'  '"${ATDM_CONFIG_SYSTEM_DIR}"'
+  ${_ASSERT_EQUALS_} '"${expected_system_dir}"'  '"${ATDM_CONFIG_CUSTOM_CONFIG_DIR}"'
 }
 
 
@@ -178,16 +200,95 @@ function test_cee_rhel6_linux_rh7() {
   assert_system_info_output dummy-rhel7 cee-rhel6 cee-rhel6
 }
 
+
 #
 # Test cases where multiple known systems are supported and test cases for
 # selecting which one
 #
 
-# ToDo: Implement!
+function test_sems_rhel7_cee_rhel6_select_sems_rhel7() {
+  init_system_info_test_env dummy-rhel7
+  SEMS_PLATFORM=rhel7-x86_64
+  SNLSYSTEM=cee
+  SNLCLUSTER=linux_rh7
+  source ${ATDM_CONFIG_SCRIPT_DIR}/utils/get_system_info.sh
+  assert_system_info_output dummy-rhel7 sems-rhel7 sems-rhel7
+}
+
+function test_sems_rhel7_cee_rhel6_select_cee_rhel6() {
+  init_system_info_test_env dummy-rhel7
+  SEMS_PLATFORM=rhel7-x86_64
+  SNLSYSTEM=cee
+  SNLCLUSTER=linux_rh7
+  ATDM_CONFIG_BUILD_NAME=cee-rhel6-default  # Have to put 'cee-rhel6' in name!
+  source ${ATDM_CONFIG_SCRIPT_DIR}/utils/get_system_info.sh
+  assert_system_info_output dummy-rhel7 cee-rhel6 cee-rhel6
+}
+
+function test_cts1_sems_sems_rhel7_select_cts1emprire() {
+  init_system_info_test_env dummy-cts1
+  SEMS_PLATFORM=rhel7-x86_64
+  SNLSYSTEM=cts1
+  SNLCLUSTER=chama-test
+  source ${ATDM_CONFIG_SCRIPT_DIR}/utils/get_system_info.sh
+  assert_system_info_output dummy-cts1 chama-test cts1empire
+}
+
+function test_cts1_sems_sems_rhel7_select_sems_rhel7() {
+  init_system_info_test_env dummy-cts1
+  SEMS_PLATFORM=rhel7-x86_64
+  SNLSYSTEM=cts1
+  SNLCLUSTER=chama-test
+  ATDM_CONFIG_BUILD_NAME=sems-rhel7-default   # Must have 'sems-rhel7' in name
+  source ${ATDM_CONFIG_SCRIPT_DIR}/utils/get_system_info.sh
+  assert_system_info_output dummy-cts1 sems-rhel7 sems-rhel7
+}
 
 
 #
 # Test use cases for custom builds
+#
+
+function test_custom_system_arg_with_default_known_system() {
+  thishost=$(hostname)
+  init_system_info_test_env ${thishost}
+  SEMS_PLATFORM=rhel7-x86_64  # Makes sure there is a known default system
+  ATDM_CONFIG_CUSTOM_CONFIG_DIR_ARG=${ATDM_UNIT_TESTS_DIR}/dummy_custom_system
+  ATDM_CONFIG_BUILD_NAME=dummy_custom_system-default # Must be in build name!
+  # Check the STDOUT
+  output=$(source ${ATDM_CONFIG_SCRIPT_DIR}/utils/get_system_info.sh)
+  assertEquals \
+    "Selecting custom system configuration 'dummy_custom_system'" \
+    "${output}"
+  # Check the vars set
+  source ${ATDM_CONFIG_SCRIPT_DIR}/utils/get_system_info.sh
+  assert_custom_system_info_output ${thishost} ${thishost} dummy_custom_system \
+    ${ATDM_CONFIG_CUSTOM_CONFIG_DIR_ARG}
+}
+
+function test_custom_system_reg_with_default_known_system() {
+  thishost=$(hostname)
+  init_system_info_test_env ${thishost}
+  SEMS_PLATFORM=rhel7-x86_64  # Makes sure there is a known default system
+  ATDM_CONFIG_REGISTER_CUSTOM_CONFIG_DIR=${ATDM_UNIT_TESTS_DIR}/dummy_custom_system
+  ATDM_CONFIG_BUILD_NAME=dummy_custom_system-default # Must be in build name!
+  # Check the STDOUT
+  output=$(source ${ATDM_CONFIG_SCRIPT_DIR}/utils/get_system_info.sh)
+  assertEquals \
+    "Selecting custom system configuration 'dummy_custom_system'" \
+    "${output}"
+  # Check the vars set
+  source ${ATDM_CONFIG_SCRIPT_DIR}/utils/get_system_info.sh
+  assert_custom_system_info_output ${thishost} ${thishost} dummy_custom_system \
+    ${ATDM_CONFIG_REGISTER_CUSTOM_CONFIG_DIR}
+}
+
+# ToDo: Add add test for ATDM_CONFIG_REGISTER_CUSTOM_CONFIG_DIR with no
+# default known system!
+
+
+#
+# Test some failure error conditions
 #
 
 # ToDo: Implement!
